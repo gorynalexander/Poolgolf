1d9220c25ae3d0935b83f067846f0d1346bf5ac0
Odinn <goryn.alexander@gmail.com>
2016-11-07 01:34:03 +0200

    Init

---
+++ Assets/ForceScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceScript : MonoBehaviour {
+
+    public Transform arrow;
+    private Transform temp;
+
+    // Use this for initialization
+    void Start () {
+        //gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector3(1,1,0));
+        temp = gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("d"))
+        {
+           // gameObject.transform.Rotate(new Vector3(0, 0, 0));
+            arrow.RotateAround(temp.position, new Vector3(0, 0, -1), 1);
+        }
+        if (Input.GetKey("a"))
+        {
+            // gameObject.transform.Rotate(new Vector3(0, 0, 0));
+            arrow.RotateAround(temp.position, new Vector3(0, 0, 1), 1);
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            // gameObject.transform.Rotate(new Vector3(0, 0, 0));
+            transform.LookAt(arrow);
+            //gameObject.GetComponent<Rigidbody2D>().AddRelativeForce(arrow.position, 0);
+
+            // gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(arrow.position.x, arrow.position.y*-1));
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Assets/RotateScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateScript : MonoBehaviour {
+    public GameObject point;
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //gameObject.transform.RotateAround(new Vector3(point.transform.position.x, point.transform.position.y, point.transform.position.z), new Vector3(0,0,-1), Mathf.SmoothStep(0, 90, Time.deltaTime * 2));
+	}
+
+    private int force = 50;
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ball")
+        {
+            Debug.Log("HUI");
+            col.gameObject.GetComponent<Rigidbody>().AddForce(force, force, 0);
+        }
+    }
+}
