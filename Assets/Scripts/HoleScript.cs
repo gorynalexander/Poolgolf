210a7af0e1d3ff5f835b2e53561e921d5cac885b
Odinn <goryn.alexander@gmail.com>
2016-11-14 02:36:34 +0200

    Ball stopping Start with different speed Start only 1 time 3 levels Animation of walls

--- Assets/Scripts/HoleScript.cs
+++ Assets/Scripts/HoleScript.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 public class HoleScript : MonoBehaviour {
 
+
+    public GameObject ball;
+
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == "Ball")
         {
             // WON
             Debug.Log("WON");
+            ball.SetActive(false);
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,44 +3,85 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    public Transform arrow;
+    public GameObject arrow;
+    public Transform upArrow;
+    public Transform downArrow;
     private Transform temp;
-    public float test;
-    // Use this for initialization
+    public float speed;
+    private bool isStarted = false;
+    private Rigidbody2D gmRigidbody;
+    public float speedVal = 1f;
+
+
+
     void Start () {
         //gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector3(1,1,0));
         temp = gameObject.transform;
+        gmRigidbody = gameObject.GetComponent<Rigidbody2D>();
 	}
 	
+
 	// Update is called once per frame
 	void Update () {
 
-        test = gameObject.GetComponent<Rigidbody2D>().velocity.sqrMagnitude;
+        speed = gmRigidbody.velocity.magnitude;
+
+        if (Input.GetKeyDown("w"))
+        {
+            arrow.transform.position = upArrow.position;
+            // this code for changing START speed +
+
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            arrow.transform.position = downArrow.position;
+            // this code for changing START speed -
+
+        }
+
         if (Input.GetKey("d"))
         {
            // gameObject.transform.Rotate(new Vector3(0, 0, 0));
-            arrow.RotateAround(temp.position, new Vector3(0, 0, -1), 1);
+            arrow.transform.RotateAround(temp.position, new Vector3(0, 0, -1), 1);
         }
         if (Input.GetKey("a"))
         {
             // gameObject.transform.Rotate(new Vector3(0, 0, 0));
-            arrow.RotateAround(temp.position, new Vector3(0, 0, 1), 1);
+            arrow.transform.RotateAround(temp.position, new Vector3(0, 0, 1), 1);
         }
 
         if (Input.GetKeyDown("space"))
         {
-            // gameObject.transform.Rotate(new Vector3(0, 0, 0));
-            //transform.LookAt(arrow);
-            gameObject.GetComponent<Rigidbody2D>().AddForce(arrow.position - gameObject.transform.position);
+            if (!isStarted)
+            {
+                gmRigidbody.AddForce((arrow.transform.position - gameObject.transform.position)*3);
+                isStarted = true;
+                arrow.SetActive(false);
+            } 
             //gameObject.GetComponent<Rigidbody2D>().AddRelativeForce(arrow.position, 0);
-            
-
             // 
-
+        }
+        if (isStarted)
+        {
+            speedVal -= 0.05f * Time.deltaTime;
         }
 
 
+        if (gmRigidbody.velocity.magnitude <= 0.8f && isStarted)
+        {
+            gmRigidbody.velocity = gmRigidbody.velocity.normalized * speedVal;
+            speedVal -= 0.05f * Time.deltaTime;
+        }
+        if (gmRigidbody.velocity.magnitude <= 1.5 && gmRigidbody.velocity.magnitude >= 0.81f && isStarted)
+        {
+            gmRigidbody.velocity = gmRigidbody.velocity.normalized * speedVal;
+            speedVal -= 0.05f * Time.deltaTime;
+        }
+        if (gmRigidbody.velocity.magnitude <= 0.4f && isStarted)
+        {
+            gmRigidbody.velocity = gmRigidbody.velocity.normalized * 0;
 
+        }
 
 
     }
